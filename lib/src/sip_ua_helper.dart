9865d7f0bde095a84eb0aa51bfa46c468934b482
Volsavr <volsavr3@.gmail.com>
2024-08-23 16:40:46 +0300

    refactoring

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -901,7 +901,6 @@ class UaSettings {
 
   bool terminateOnMediaPortZero = false;
 
-
   /// Sip Message Delay (in millisecond) (default 0).
   int sip_message_delay = 0;
   List<Map<String, String>> iceServers = <Map<String, String>>[
