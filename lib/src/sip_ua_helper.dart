024e7a5a8903f794a184df49f4dff545b7745b81
Volsavr <volsavr3@.gmail.com>
2024-09-09 17:38:02 +0300

    Update sip_ua_helper.dart

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -922,8 +922,7 @@ class UaSettings {
 
   /// Min interval between recovery connection, default 2 sec
   int connectionRecoveryMinInterval = 2;
-
-
+  
   /// Allows to write advanced call statistics in the log after the call ends
   bool logCallStatistics = false;
 
