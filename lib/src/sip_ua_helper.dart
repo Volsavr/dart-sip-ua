621a133c382860cd880cbcc56735801742a0cbf1
Volsavr <volsavr3@.gmail.com>
2024-09-09 17:04:02 +0300

    pingInterval implementation

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -817,6 +817,9 @@ class WebSocketSettings {
   /// Otherwise the used protocol will be used (for example WS for ws://
   /// or WSS for wss://, based on the given web socket URL).
   String? transport_scheme;
+
+  /// The interval to send ping message to server.
+  Duration? pingInterval;
 }
 
 class TcpSocketSettings {
@@ -928,8 +931,11 @@ class UaSettings {
   /// Min interval between recovery connection, default 2 sec
   int connectionRecoveryMinInterval = 2;
 
+
   /// Allows to write advanced call statistics in the log after the call ends
   bool logCallStatistics = false;
+
+  Duration webSocketPingInterval = webSocketSettings.pingInterval;
 
   bool terminateOnMediaPortZero = false;
 
