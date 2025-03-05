535c91c1d7c8845fe77e2131e8904c0d9d9fb631
Volodymyr B <volodymyr.b@didww.com>
2025-03-05 17:04:04 +0200

    added ice srflx candidate policy

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -63,6 +63,7 @@ class Settings {
 
   /// ICE Gathering Timeout (in millisecond).
   int ice_gathering_timeout = 500;
+  bool ice_srflx_candidate_policy = false;
 
   /// Call statistics in the log
   bool log_call_statistics = false;
@@ -76,7 +77,7 @@ class Settings {
 // Configuration checks.
 class Checks {
   Map<String, Null Function(Settings src, Settings? dst)> mandatory =
-      <String, Null Function(Settings src, Settings? dst)>{
+  <String, Null Function(Settings src, Settings? dst)>{
     'sockets': (Settings src, Settings? dst) {
       List<SIPUASocketInterface>? sockets = src.sockets;
 
@@ -116,7 +117,7 @@ class Checks {
     }
   };
   Map<String, Null Function(Settings src, Settings? dst)> optional =
-      <String, Null Function(Settings src, Settings? dst)>{
+  <String, Null Function(Settings src, Settings? dst)>{
     'authorization_user': (Settings src, Settings? dst) {
       String? authorization_user = src.authorization_user;
       if (authorization_user == null) return;
@@ -258,6 +259,9 @@ class Checks {
     },
     'ice_gathering_timeout': (Settings src, Settings? dst) {
       dst!.ice_gathering_timeout = src.ice_gathering_timeout;
+    },
+    'ice_srflx_candidate_policy': (Settings src, Settings? dst) {
+      dst!.ice_srflx_candidate_policy = src.ice_srflx_candidate_policy;
     },
     'log_call_statistics': (Settings src, Settings? dst) {
       dst!.log_call_statistics = src.log_call_statistics;
